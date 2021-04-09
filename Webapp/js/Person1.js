fe2530c07f49a54a13fb07b78df7a9e7b54963a3
Klejdi <klejdi@ymail.com>
2021-04-08 22:56:25 -0500

    lab11added

--- Webapp/js/Person1.js
+++ Webapp/js/Person1.js
@@ -1,5 +1,5 @@
  import { person } from "./sub-sub-folder/person.js";
- var emp = new employee();
+
  var p1 = new person("Ana Smith", new Date("1998-12-15").toISOString().substring(0, 10));
  var p2 = new person("Bob Jone", new Date("1945-11-16").toISOString().substring(0, 10));
  var p3 = new person("Carlos Slim Helu", new Date("1976-09-24").toISOString().substring(0, 10));
--- lab11a/js/main.js
+++ lab11a/js/main.js
@@ -1,34 +1,65 @@
 var row = 2;
+
+function sbmt() {
+    event.preventDefault();
+    let AccNo = $("#acc").val();
+    let Cname = $("#cname").val();
+    let TypeAccount = $("#opt").val();
+    // if ($.trim($("acc").html()) == '' || $.trim($("#cname").html()) == '') {
+    //     $('#errormsg').text("Please fill the form").css("color", "red");
+    // } else {
+    var newrow = "<tr> <th scope='row'>" + ++row + "</td> <td>" + AccNo + "</td>  <td>" + Cname + "</td>  <td class='ishide'>" + TypeAccount + "</td> </tr>";
+    $('tbody').append(newrow);
+}
+
+// }
+
 $(function() {
-
-
+    // event.preventDefault();
+    //var row = 2;
     // function CreateNewAccount() {
 
-    $("#create").click(function() {
+    // $("#create").click(function(event) {
+    //     event.preventDefault();
+    //     let AccNo = $("#acc").val();
+    //     let Cname = $("#cname").val();
+    //     let TypeAccount = $("#opt").val();
+
+    //     var newrow = "<tr> <th scope='row'>" + ++row + "</td> <td>" + AccNo + "</td>  <td>" + Cname + "</td>  <td class='ishide'>" + TypeAccount + "</td> </tr>";
+    //     $('tbody').append(newrow);
 
 
-        let AccNo = $("#acc").val();
-        let Cname = $("#cname").val();
-        let TypeAccount = $("#opt").val();
-        // console.log(AccNo, Cname, TypeAccount);
 
-        var newrow = "<tr> <th scope='row'>" + ++row + "</td> <td>" + AccNo + "</td>  <td>" + Cname + "</td>  <td class='ishide'>" + TypeAccount + "</td> </tr>";
-        $('tbody').append(newrow);
-    });
-    var isHideElder = false;
+    // });
+    // var isHideElder = false;
     $("#chck:checkbox").change(function() {
-        isHideElder = !isHideElder;
-        var e = $('.ishide');
-        for (i = 0; i < e.length; i++) {
-            if (e[i].textContent == "Checking") {
 
-                if (!isHideElder) {
-                    e[i].parentElement.removeAttribute("style");
-                } else {
-                    e[i].parentElement.setAttribute("style", "display:none");
-                }
-            }
+
+
+        if (!this.checked) {
+
+
+            $('#display').find('tbody tr').show();
+            //$('#display td:nth-child(4):contains("Loan")').parent().hide();
+            // e[i].parent().hide();
+
         }
+        if (this.checked) {
+            // $('#display').find('tbody tr:not(:contains("Loan"))').hide();
+            $('#display td:nth-child(4):not(:contains("Saving"))').parent().hide();
+        }
+        // isHideElder = !isHideElder;
+        // var e = $('.ishide');
+        // for (i = 0; i < e.length; i++) {
+        //     if (e[i].textContent == "Checking") {
+
+        //         if (!isHideElder) {
+        //             e[i].parentElement.removeAttribute("style");
+        //         } else {
+        //             e[i].parentElement.setAttribute("style", "display:none");
+        //         }
+        //     }
+        // }
 
         // for (i = 0; i < ele.length; i++) {
         //     if (ele[i].html == "Saving")
@@ -37,20 +68,33 @@ $(function() {
 
 
     });
-    var isHideElder = false;
+    // var isHideElder = false;
     $("#chck1:checkbox").change(function() {
-        isHideElder = !isHideElder;
-        var e = $('.ishide');
-        for (i = 0; i < e.length; i++) {
-            if (e[i].textContent == "Saving") {
 
-                if (!isHideElder) {
-                    e[i].parentElement.removeAttribute("style");
-                } else {
-                    e[i].parentElement.setAttribute("style", "display:none");
-                }
-            }
+        if (!this.checked) {
+
+
+            $('#display').find('tbody tr').show();
+            //$('#display td:nth-child(4):contains("Loan")').parent().hide();
+            // e[i].parent().hide();
+
         }
+        if (this.checked) {
+            // $('#display').find('tbody tr:not(:contains("Loan"))').hide();
+            $('#display td:nth-child(4):not(:contains("Checking"))').parent().hide();
+        }
+        // isHideElder = !isHideElder;
+        // var e = $('.ishide');
+        // for (i = 0; i < e.length; i++) {
+        //     if (e[i].textContent == "Saving") {
+
+        //         if (!isHideElder) {
+        //             e[i].parentElement.removeAttribute("style");
+        //         } else {
+        //             e[i].parentElement.setAttribute("style", "display:none");
+        //         }
+        //     }
+        // }
 
         // for (i = 0; i < ele.length; i++) {
         //     if (ele[i].html == "Saving")
@@ -63,17 +107,32 @@ $(function() {
     $("#chck2:checkbox").change(function() {
 
 
+        //----------------------It'S not working for no reason------------------------
+        // if (this.checked) {
+        //     var ele = $('.ishide'); // -->Created a class for every typeof account we add or we have
+        //     for (i = 0; i < ele.length; i++) { //loop through 
+        //         if (ele[i].textContent == "Loan")
+        //             console.log(ele[i]);
+        //         // ele.parent().hide(); //-->if we wanna hide all of them it works
+        //         //ele[i].parent().hide(); //-->is not working
+        //     }
+        // } else {
+        //     ele.parent().show();
+        // }
+
+        //----------------------------------------------------------------------------
 
         if (!this.checked) {
 
 
-            $('#display').find('tbody tr:contains("Loan")').show();
+            $('#display').find('tbody tr').show();
             //$('#display td:nth-child(4):contains("Loan")').parent().hide();
             // e[i].parent().hide();
 
         }
         if (this.checked) {
-            $('#display').find('tbody tr:contains("Loan")').hide();
+            // $('#display').find('tbody tr:not(:contains("Loan"))').hide();
+            $('#display td:nth-child(4):not(:contains("Loan"))').parent().hide();
         }
 
 
